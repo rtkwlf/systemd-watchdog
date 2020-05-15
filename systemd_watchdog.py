e0dededd08ed30449aa3f7cd8ddf4da6d736245d
Aaron D. Marasco <systemd-watchdog@marascos.net>
2020-05-14 22:52:06 -0400

    Alphabetize

--- systemd_watchdog.py
+++ systemd_watchdog.py
@@ -9,11 +9,11 @@ import socket
 
 class watchdog():
     def __init__(self, sock=None, addr=None):
+        self._address = addr or os.getenv("NOTIFY_SOCKET")
+        self._lastcall = datetime.fromordinal(1)
         self._socket = sock or socket.socket(family=socket.AF_UNIX,
                                              type=socket.SOCK_DGRAM)
-        self._address = addr or os.getenv("NOTIFY_SOCKET")
         self._timeout_td = timedelta(0)
-        self._lastcall = datetime.fromordinal(1)
 
         # Note this fix is untested in a live system; https://unix.stackexchange.com/q/206386
         if self._address and self._address[0] == '@':
@@ -28,7 +28,7 @@ class watchdog():
                     self._timeout_td = timedelta(microseconds=int(wtime))
 
     def _send(self, msg):
-        """Send string `msg` as bytes on the notification socket"""
+        """Send string as bytes on the notification socket"""
         if self.is_enabled:
             self._socket.sendto(msg.encode(), self._address)
 
@@ -48,19 +48,21 @@ class watchdog():
 
     def notify_error(self, msg=None):
         """
-        Report a watchdog error. This program will likely be killed by the
-        service manager.
+        Report a watchdog error. This program will likely be killed by the service manager.
 
-        If `msg` is provided, it will be reported as a status message prior to the error.
+        If msg is provided, it will be reported as a status message prior to the error.
         """
         if msg:
             self.status(msg)
-
         self._send("WATCHDOG=trigger\n")
 
     def ready(self):
-        """Report ready service state, i.e. completed initialisation"""
+        """Report ready service state, i.e. completed init"""
         self._send("READY=1\n")
+
+    def status(self, msg):
+        """Set a service status message"""
+        self._send("STATUS=%s\n" % (msg,))
 
     @property
     def timeout(self):
@@ -71,7 +73,3 @@ class watchdog():
     def timeout_td(self):
         """Report the watchdog window as datetime.timedelta (cf. sd_watchdog_enabled(3) )"""
         return self._timeout_td
-
-    def status(self, msg):
-        """Set a service status message"""
-        self._send("STATUS=%s\n" % (msg,))
